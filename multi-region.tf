42b672619ca2ea88dc87f622376758df7680afc9
Athira <sivamathira@yahoo.com>
2025-07-03 13:27:19 -0400

    multi-region infrastructure code

---
+++ multi-region.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  alias = "region1"
+  region = "us-east-1"
+}
+
+provider "aws" {
+    alias = "region2"
+    region = "us-east-2"
+  
+}
+
+resource "aws_instance" "example1" {
+    ami = "ami-020cba7c55df1f615"
+    instance_type = "t3.micro"
+    provider = "aws.region1"
+    tags = {
+        Name = "demo1"
+    }
+}
+resource "aws_instance" "example2" {
+    ami = "ami-0d1b5a8c13042c939"
+    instance_type = "t3.micro"
+    provider = "aws.region2"
+    tags = {
+        Name = "demo2"
+    }
+}
