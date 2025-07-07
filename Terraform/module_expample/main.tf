b8677385d924505cea645aaf99abb2b5efe7f5bc
Athira Sivadas <sivamathira@yahoo.com>
2025-07-07 14:21:45 -0400

    created terraform module and resused it

---
+++ Terraform/module_expample/main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ec2_instance" {
+  source = "./modules/ec2_instance"
+  ami_value = "ami-020cba7c55df1f615"
+  instance_type_value = "t2.micro"
+}
---
+++ Terraform/module_expample/terraform.tfvars
@@ -0,0 +1,2 @@
+ami_value = "ami-020cba7c55df1f615"
+instance_type_value = "t2.micro"
